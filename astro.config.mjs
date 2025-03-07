ab69e56a8303801be7157717631bc87e4ef36d4b
oussamachah2020 <otaksam213@gmail.com>
2025-03-07 17:07:57 +0000

    modify ip address

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
+  output: "server", // Enable SSR
+  adapter: node({
+    mode: "standalone",
+  }),
 });
